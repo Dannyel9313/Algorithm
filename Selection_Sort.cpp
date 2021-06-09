9463d3966ad965284a095eecfecfa1d08a1aa67e
Daniel Sakalev <daniel.sakalev@egt-bg.com>
2021-06-09 07:42:22 +0300

    implement selection sort function in ascending order

---
+++ Selection_Sort.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <limits.h>
+
+
+// Selection sort in ascending order
+void SortInAscendingOrder(std::vector<int>& vec)
+{
+    for(size_t i = 0; i < vec.size()-2; i ++)
+    {
+        int smallest = i;
+        for(size_t j = i + 1; j <= vec.size() - 1; j++ )
+        {
+            if( vec.at(j) < vec.at(smallest))
+            {
+                smallest = j;
+            }
+        }
+        if(vec.at(i) > vec.at(smallest))
+        {
+            std::swap(vec.at(i),vec.at(smallest));
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+
+    std::vector<int> test = { 5, 6, 8, 1, 3 };
+    SortInAscendingOrder(test);
+    for(auto& elem : test)
+    {
+        std::cout << elem << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
