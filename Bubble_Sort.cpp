e2e44891f5f82b366169ef9b456ef795a5a75a3c
Daniel Sakalev <daniel.sakalev@egt-bg.com>
2021-06-09 09:01:30 +0300

    added optimized version of bubble sort for already sorted array

--- Bubble_Sort.cpp
+++ Bubble_Sort.cpp
@@ -19,10 +19,38 @@ void SortInAscendingOrder(std::vector<int>& vec)
     }
 }
 
+// optimised bubble sort algorithm when has a already sorted vector to be 0(n)
+// if vector is already sorted time complexity is 0(n)
+void OptimizedSortInAscendingOrder(std::vector<int>& vec)
+{
+    for(size_t count = 0; count < vec.size() - 1; count ++)
+    {
+        bool hasSwap = false;
+        for(size_t j = 0; j <= vec.size() - 2; j++)
+        {
+            if(vec.at(j) > vec.at(j+1))
+            {
+                std::swap(vec.at(j),vec.at(j + 1));
+                hasSwap = true;
+            }
+        }
+        // break if has no swap in first iteration vector is already sorted
+        if(!hasSwap)
+        {
+            break
+        }
+    }
+}
+
+
 int main(int argc, char *argv[])
 {
     std::vector<int> test = {4, 8, 9, 1, 3};
     SortInAscendingOrder(test);
+
+    std::vector<int> alreadySorted = {1, 3, 5, 6, 7};
+    OptimizedSortInAscendingOrder(alreadySorted);
+
     for(const auto& elem : test)
     {
         std::cout << elem << " ";
